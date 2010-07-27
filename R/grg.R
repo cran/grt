cb694415d373fcab5f695524dd3600c059dc1531
Kazunaga Matsuki <kmatsuki@uwo.ca>
2010-07-27 00:00:00 +0000

    version 0.1.0

---
+++ R/grg.R
@@ -0,0 +1,20 @@
+grg <- function(response, fixed = FALSE, k = 2)
+{
+    response <- as.vector(response)
+    N <- length(response)
+    n <- table(response)
+    if(length(n) > 2) stop("more than 2 categories found")
+    fit <- NULL
+    if(fixed){
+        fit$logLik <- N * log(fit$par <- .5)
+        attr(fit$logLik, "df") <- 0
+    } else {
+        m <- max(n)
+        fit$par <- p <- m/N
+        fit$logLik <- m*log(p) + (N-m)*log(1-p)
+        attr(fit$logLik, "df") <- 1
+    }
+    class(fit$logLik) <- "logLik"
+    fit$AIC <- AIC(fit$logLik, k = k)
+    fit
+}
